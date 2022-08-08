823c3aa1fff1382a6632d1f36c8ec4af37a89877
André Donadon <andre.donadon@outlook.com>
2022-08-07 23:31:26 -0300

    Add Lombok

--- src/main/java/io/github/andredonadon/quarkussocial/domain/model/User.java
+++ src/main/java/io/github/andredonadon/quarkussocial/domain/model/User.java
@@ -1,16 +1,17 @@
 package io.github.andredonadon.quarkussocial.domain.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Objects;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User extends PanacheEntityBase {
 
     @Id
@@ -18,41 +19,4 @@ public class User extends PanacheEntityBase {
     private Long id;
     private String name;
     private Integer age;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id.equals(user.id) && name.equals(user.name) && age.equals(user.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
--- src/main/java/io/github/andredonadon/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/andredonadon/quarkussocial/rest/dto/CreateUserRequest.java
@@ -1,28 +1,15 @@
 package io.github.andredonadon.quarkussocial.rest.dto;
 
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Data
 public class CreateUserRequest {
 
     @NotBlank(message = "name is required")
     private String name;
     @NotNull(message = "age is required")
     private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
